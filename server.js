d710e43fafa64f82a3d7309cb6665a1c3b114cd0
Glen Westbrook <mgw@tutanota.com>
2016-11-24 22:35:40 +0000

    Add index page at app root

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express();
 
 app.set('view engine', 'pug');
 
-app.use(express.static('client'));
-app.use(headerParser);
+app.use(express.static('public'));
+app.use('/api/whoami', headerParser);
+
+app.get('/', (req, res) => res.render('index', { host: req.host }));
 
 const listener = app.listen(process.env.PORT, function() {
     console.log(`Server listening on port: ${listener.address().port}`);
